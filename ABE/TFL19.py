748b3b1ecc4004ac030e8b6b3a130f8045b901b2
Afonso Pimentel <afonso.am99@gmail.com>
2022-10-30 16:41:09 +0000

    Added Files

---
+++ ABE/TFL19.py
@@ -0,0 +1,235 @@
+'''
+Yi-Fan Tseng, Chun-I Fan, Zi-Cheng Liu
+ 
+| From: "Fast Keyword Search Over Encrypted Data with Short Ciphertext in Clouds"
+| Published in: 
+| Available from: 
+| Notes: Security Assumption: DBDH on asymmetric paring group. 
+
+* type:           SE from KPABE
+* setting:        Pairing
+
+:Authors:    Yi-Fan Tseng
+:Date:            08/30/2019
+'''
+from charm.toolbox.pairinggroup import PairingGroup,ZR,G1,G2,GT,pair
+from charm.toolbox.secretutil import SecretUtil
+from charm.toolbox.ABEnc import ABEnc
+from charm.toolbox.hash_module import Waters, Hash
+import time
+
+debug = False
+class TFL19(ABEnc):
+    
+    def __init__(self, groupObj):
+        ABEnc.__init__(self)
+        global util, group
+        util = SecretUtil(groupObj, debug)        
+        group = groupObj
+
+    def setup(self):
+        g1, g2 = group.random(G1), group.random(G2)
+        alpha, beta, phi, m = group.random(), group.random(), group.random(), group.random(GT)      
+        h = g1 ** phi
+        h_hat = g2 ** phi
+        U = pair(g1, g2) ** (alpha * (beta - 1))
+        V = pair(g1, g2) ** (alpha * beta)
+               
+        pk = {'g1':g1, 'g2':g2, 'U':U, 'V':V, 'h':h, 'm':m}
+        msk = pk.copy()
+        msk["g_hat^alpha"] = g2 ** alpha
+        msk["h_hat"] = h_hat
+        self.msk = msk
+
+        return (msk, pk)
+
+    def s_keygen(self, pk):
+        return (pk, self.msk)
+
+    def encrypt(self, pk, Attribute):
+        H = Hash(group)
+        k = group.random()
+
+        CT = dict()
+        CT["M"] = pk["m"]
+        CT["C1"] = pk["m"] * pk["V"]** k
+        CT["C2"] = pk["U"] ** k
+        CT["C3"] = pk["g1"] ** k
+
+        for name, value in Attribute.items():
+            r = H.hashToZr(name + value)
+            CT[name] = (pk["h"] * pk["g1"] ** r)**k
+        
+        return CT
+    
+    def keygen2(self, msk, pk, Pol, Pol_M):
+        H = Hash(group)
+        SK = list()
+        s, y2, y3 = 1, group.random(), group.random()
+        yVector = [s] + [group.random() for i in range(len(Pol)-1)]
+
+        sigma = dict()
+        for dictEntryP in Pol:
+            name, value = next(iter(dictEntryP.items()))
+            h = H.hashToZr(name + value)
+            sigma[name] = msk["h_hat"] * msk["g2"]**h
+        
+        for dictEntryM in Pol_M:
+            name, row = next(iter(dictEntryM.items()))
+            #lamb = row[0] + row[1]*y2 + row[2]*y3
+            lamb = 0
+            for r, y in zip(row, yVector):
+                lamb += r*y
+            
+            r = group.random()
+            
+            Q = dict()
+            D0 = (msk["g_hat^alpha"]**lamb) * (sigma[name] ** r)
+            D1 = msk["g2"]**r
+            tmp = [en for en in Pol if name not in en]
+            #del tmp[name]
+            Q = []
+            for j in tmp:
+                nameAux, _ = next(iter(j.items()))
+                Q.append(sigma[nameAux] ** r)
+            SK.append({"D0":D0, "D1":D1, "Q":Q})
+
+        return SK
+    
+    def keygen(self, msk, pk, Pol, Pol_M, weights):
+        H = Hash(group)
+        SK = list()
+        s, y2, y3 = 1, group.random(), group.random()
+        yVector = [s] + [group.random() for i in range(len(Pol)-1)]
+
+        sigma = dict()
+        for dictEntryP in Pol:
+            name, value = next(iter(dictEntryP.items()))
+            h = H.hashToZr(name + value)
+            sigma[name] = msk["h_hat"] * msk["g2"]**h
+        
+        for dictEntryM in Pol_M:
+            name, row = next(iter(dictEntryM.items()))
+            #lamb = row[0] + row[1]*y2 + row[2]*y3
+            lamb = 0
+            for r, y in zip(row, yVector):
+                lamb += r*y
+            
+            r = group.random()
+            
+            Q = dict()
+            D0 = (msk["g_hat^alpha"]**lamb) * (sigma[name] ** r)
+            D1 = msk["g2"]**r
+            tmp = [en for en in Pol if name not in en]
+            #del tmp[name]
+            Q = []
+            for j in tmp:
+                nameAux, _ = next(iter(j.items()))
+                Q.append(sigma[nameAux] ** r)
+            SK.append({"D0":D0, "D1":D1, "Q":Q})
+        SK2 = list()
+        for query in weights:
+            L = numerator = denominator = 1
+            for i in query:
+                tmp = SK[i]["D0"]
+                for j in query:
+                    aux = j
+                    if j > i:
+                        aux -= 1
+                    if j != i:
+                        tmp *= SK[i]["Q"][aux]
+                numerator *= tmp
+                denominator *= SK[i]["D1"]
+            SK2.append({'numerator':numerator, 'denominator':denominator})
+
+        return SK2
+
+    def decrypt(self, pk, sk_s, SK, CT, Delta, weights = [[]]):
+        for query, sk_i in zip(weights, SK):
+            L = numerator = denominator = 1
+            delta = [Delta[i] for i in query]
+            for name in delta:
+                L *= CT[name]
+
+            Z = (pair(CT["C3"], sk_i['numerator'])) / (pair(L, sk_i['denominator']))
+            m = CT["C1"] / (CT["C2"] * Z)
+            if CT["M"] == m:
+                return True
+        return False
+
+
+    def decrypt2(self, pk, sk_s, SK, CT, Delta, weights = [[]]):
+        for query in weights:
+            L = numerator = denominator = 1
+            delta = [Delta[i] for i in query]
+            for name in delta:
+                L *= CT[name]
+            for i in query:
+                tmp = SK[i]["D0"]
+                for j in query:
+                    aux = j
+                    if j > i:
+                        aux -= 1
+                    if j != i:
+                        tmp *= SK[i]["Q"][aux]
+                numerator *= tmp
+                denominator *= SK[i]["D1"]
+            Z = (pair(CT["C3"], numerator)) / (pair(L, denominator))
+            m = CT["C1"] / (CT["C2"] * Z)
+            if CT["M"] == m:
+                return True
+        return False
+
+    def test(self):
+        a, a1 = group.random(G1,2)
+        b, b1 = group.random(G2,2)
+        n, n1 = group.random(ZR,2)
+        start = time.time()
+        c = pair(a, b)
+        end = time.time()
+        print('Pairing Time: ', (end - start)*1000)
+        c1 = pair(a1, b1)
+        start = time.time()
+        a * n1
+        end = time.time()
+        print('Div Time: ', (end - start)*1000)
+        return 
+
+def main():
+    #Get the eliptic curve with the bilinear mapping feature needed.
+    groupObj = PairingGroup('MNT159')
+    #groupObj = PairingGroup('BN254')
+    #groupObj = PairingGroup('SS512')
+
+    #Policy_Matrix = [{'School':[1, 1, 0]}, {"Pos":[0, -1, 0]}]
+    #Policy = [{'School':"NSYSU"}, {"Pos":"Teacher"}]
+    #Attribute = {"School":"NSYSU", "Pos":"Teacher"}
+    #Weights = [[0, 1]]
+    #Delta = ["School", "Pos"]
+    Policy_Matrix = [{'col3': [1, 0]}, {'col0': [1, 1]}, {'col1': [0, -1]}]
+    Policy = [{'col3': '0'}, {'col0': '4'}, {'col1': '0'}]
+    Attribute = {'col0': '5', 'col1': '0', 'col2': '3', 'col3': '0', 'col4': '0'}
+    Delta =  ['col3', 'col0', 'col1']
+    Weights = [[1, 2], [0]]
+    
+    #test_time = 100
+
+    kpabe = TFL19(groupObj)
+    kpabe.test()
+    (msk, pk) = kpabe.setup()
+    #m = group.random(GT)
+    start = time.time()
+    CT = kpabe.encrypt(pk, Attribute)
+    end = time.time()
+    print("Encryption: ", (end - start)*1000)
+    SK = kpabe.keygen2(msk, pk, Policy, Policy_Matrix, Weights)
+    start = time.time()
+    msg = kpabe.decrypt2(pk, "lixo", SK, CT, Delta, Weights)
+    end = time.time()
+    print("Decryption: ", (end - start)*1000)
+    print(msg)
+
+
+if __name__ == '__main__':
+    debug = True
+    main()
---
+++ VANET/runSimulations.py
@@ -0,0 +1,173 @@
+import csv
+import subprocess, shlex
+from math import ceil
+from os import path
+from functools import partial
+import concurrent.futures
+import multiprocessing
+
+class SEScheme:
+    
+    def parseEq(self, dictEquation):
+        pars = dict()
+        for name in dictEquation:
+            eqs = dictEquation[name].replace(" ", "").split("+")
+            for eq in eqs:
+                if "*" in eq:
+                    nbr, par = eq.split("*", 1)
+                else:
+                    nbr, par = eq, "c"
+                pars[name + "_" + par] = float(nbr)
+        return pars
+        
+    #initialize an algorithm with attributes 
+    def __init__(self, equations, ident):
+        self.attr = self.parseEq(equations)
+        self.id = ident
+
+    def getAttrs(self, algName, values):
+        inf = algName + "_" 
+        return [self.attr[inf + val] if inf + val in self.attr else 0 for val in values] 
+
+    def dotProd(self, lst1, lst2):
+        return sum(a*b for a, b in zip(lst1, lst2))
+    
+    #computes time and space that a given algorithm will take
+    def getAlgMeasures(self, algName, nbrKey, nbrDisj=0, nbrLin=1):
+        inputs = [nbrKey**2, nbrKey, nbrDisj**2, nbrDisj, 1]
+        tAttr = self.getAttrs(algName + "_T", ["x2", "x", "y2", "y", "c"])
+        sAttr = self.getAttrs(algName + "_S", ["x2", "x", "y2", "y", "c"])
+        time = self.dotProd(tAttr, inputs)
+        space = self.dotProd(sAttr, inputs)
+        if algName == "enc" or algName == "tst":
+            time *= nbrLin
+            space *= nbrLin
+        return time, space
+
+pattern = "\n===BEGIN SIMULATION===\n"
+
+def simGetInt(valId, simOutput, extra = ""):
+    rightPartition = simOutput.split(valId)[1]
+    return {valId[:-2] + extra: int(rightPartition.split("\n")[0])}
+
+
+def getSimValues(pattern, simEntry, portsApp):
+    #print(simEntry)
+    simResults = simEntry.split(pattern)[1]
+    simCleanEntry=  simResults.split("Info on App with destination port:")[1:]
+    lostPackets, rcvPackets, avgDelay = {}, {}, {}
+    for sim in simCleanEntry:
+        port = int(sim.split("\n")[0])
+        lostPackets.update(simGetInt("Total Lost Packets: ", sim, portsApp[port]))
+        rcvPackets.update(simGetInt("Total Received Packets: ", sim, portsApp[port]))
+        avgDelay.update(simGetInt("Average Delay (ms): ", sim, portsApp[port]))
+    return {**lostPackets, **rcvPackets, **avgDelay}
+
+def cmdFunc(cmd):
+    print(cmd)
+    process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, _ = process.communicate()
+    return out.decode('utf-8')
+
+#receive inputs that need to be computed
+def doOneMeasure(portApp, input):
+    cmd = lambda x : f"./waf --run-no-build \"examples/NS3Scenario/vanet-test --numNodes=" + x["numNodes"] + " --procTime1=" + x["procTime1"] + " --nPackets1=" + x["nPackets1"] + " --procTime2=" + x["procTime2"] + " --nPackets2=" + x["nPackets2"] +" --mobTrace=" + x["seed"] + " --scenario=" + x["scenario"]+ "\""
+    #cmd = lambda x : f"./waf --run-no-build \"examples/NS3Scenario/vanet-test --numNodes=" + x["numNodes"] + " --procTime1=" + x["procTime1"] + " --nPackets1=" + x["nPackets1"] + " --mobTrace=" + x["seed"] + " --scenario=" + x["scenario"]+ "\""
+    simResutlt = cmdFunc(cmd(input))
+    output = getSimValues(pattern, simResutlt, portApp)
+    return {**input, **output}
+
+
+
+def measureAndWrite(portsApp, folder, filename, measure):
+    val = doOneMeasure(portsApp, measure)
+    writeMeasure(val, folder, filename)
+
+def readMeasures(folder, filename):
+    if path.exists(folder + "/" + filename + ".csv"):
+        with open(folder + "/" + filename + ".csv", mode="r") as file:
+            writer = csv.DictReader(file)
+            return list(writer)
+    return []
+
+#removes measures already done from inputs
+def alreadyMeasure(doneMeasures, inputs):     
+    for g in doneMeasures:
+        for inp in inputs:
+            if inp.items() <= g.items():
+                inputs.remove(inp)
+                break
+
+def getInputsScenario1(scheme, numKeywords, numD, nLin):
+    pT, nP = scheme.getAlgMeasures("enc", numKeywords, nbrLin=nLin)
+    return {"nPackets1": str(ceil(nP / 1.024)), "procTime1": str(pT*1000)}
+
+
+def getInputsScenario2(scheme, numKeywords, numDisj, nLin):
+    pT1, nP1 = scheme.getAlgMeasures("gT", numKeywords, numDisj)
+    pT2, _ = scheme.getAlgMeasures("tst", numKeywords, numDisj, nLin)
+    _, sizeEnc = scheme.getAlgMeasures("enc", numKeywords, nbrLin=nLin)
+    nP2 = ceil(sizeEnc / 10.24)
+    nP1 = ceil(nP1 / 1.024)
+    return {"nPackets1":str(nP1), "procTime1":str(pT1*1000),  "nPackets2":str(nP2), "procTime2":str(pT2*1000)}
+
+
+def writeMeasures(measures, folder, filename):
+    keys = measures[0].keys()
+    print("MEASURES")
+    print(keys)
+    if not path.exists(folder + "/" + filename + ".csv"):
+        with open(folder + "/" +filename + ".csv", mode="w") as file:
+            dict_writer = csv.DictWriter(file, keys)
+            dict_writer.writeheader()
+            dict_writer.writerows(measures)
+    else:
+        with open(folder + "/" + filename + ".csv", mode="a") as file:
+            dict_writer = csv.DictWriter(file, keys)
+            dict_writer.writerows(measures)
+
+def main():
+    
+    portsApp = {48: "1", 15: "2"}
+    #portsApp = {49153: "1"}
+    schemePlain = {"gT_T": "0.001", "enc_T" : "0.001", "tst_T" : "0.000001", "gT_S" : "1", "enc_S": "0.008*x"}
+    scheme1 = {"gT_T": "0.01*x2 + 0.003*x", "enc_T" : "0.003*x+0.027", "tst_T" : "0.105*y+0.113", "gT_S" : "0.337*y+0.533", "enc_S": "0.074*x + 2.078"}
+    scheme2 = {"gT_T": "0.004*x", "enc_T" : "0.057*x+0.014", "tst_T" : "0.054*y+0.057", "gT_S" : "0.179*y+0.447", "enc_S": "0.781*x + 0.307"}
+    sePlain = SEScheme(schemePlain, 0)
+    seScheme1 = SEScheme(scheme1, 1)
+    seScheme2 = SEScheme(scheme2, 2)
+    scheme = [sePlain, seScheme1, seScheme2]
+    numNodes = [58]
+    numDisjunctions = [1, 2]
+    #numKeywords = [1, 5, 10, 15, 30, 50, 100]
+    numKeywords = [10]
+    #numLines = [10]
+    numLines = [75, 125, 150, 175]
+    seed = [1 ,2, 3, 4, 5, 6, 7, 8, 9, 10]
+    inputs = []
+
+    for sc in scheme:
+        for nN in numNodes:
+            for nL in numLines:
+                for nK in numKeywords:
+                    for nD in numDisjunctions:
+                        for s in seed:
+                            parm = getInputsScenario2(sc, nK, nD, nL)
+                            inp = {"scheme" : str(sc.id), "numNodes" : str(nN), "numKeywords" : str(nK), "seed": str(s), "scenario" : str(2), "numLines" : nL, "numDisjunctions": nD}
+                            inputs.append({**inp, **parm})
+    
+    doneMeasures = readMeasures("measuresTest","test10")
+    alreadyMeasure(doneMeasures, inputs)
+    print(inputs)
+    funcThread2 = partial(doOneMeasure, portsApp)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executor:
+        results = executor.map(funcThread2, inputs)
+    
+    print("Hello changes")
+    lstRes = list(results)
+    print(lstRes)
+
+    if len(lstRes) > 0:
+        print("HI")
+        writeMeasures(lstRes,"measuresTest","test10" )
+main()
